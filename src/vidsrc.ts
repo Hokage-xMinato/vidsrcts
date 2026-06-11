8e0f0e407aa1bd3c279d57bdeb979f0efeacf222
Codingworld <minatonamikaze4756@gmail.com>
2026-06-11 10:34:36 +0530

    Update vidsrc.ts

--- src/vidsrc.ts
+++ src/vidsrc.ts
@@ -106,8 +106,8 @@ async function tmdbScrape(tmdbId: string, type: "movie" | "tv", season?: number,
     throw new Error("Invalid Data.");
   }
   const url = (type === "movie")
-    ? `https://vidsrc.net/embed/${type}?tmdb=${tmdbId}`
-    : `https://vidsrc.net/embed/${type}?tmdb=${tmdbId}&season=${season}&episode=${episode}`;
+    ? `https://vidsrcme.ru/embed/${type}?tmdb=${tmdbId}`
+    : `https://vidsrcme.ru/embed/${type}?tmdb=${tmdbId}&season=${season}&episode=${episode}`;
   const embed = await fetch(url);
   const embedResp = await embed.text();
 
